f5c5b6202af2ace1676943e077b61b87f3f9de96
TheWatcherMultiversal <98140588+TheWatcherMultiversal@users.noreply.github.com>
2022-07-29 18:52:51 -0500

    Update veapysh_viewprocess.sh

--- Configuration/veapysh_viewprocess.sh
+++ Configuration/veapysh_viewprocess.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# This is a simple script to check active processes.
 # Only modify the source code if you know what you are doing.
 
 # Developed by: Angel Gabriel Mortera Gual.
@@ -9,9 +6,9 @@
 echo ""
 echo 'Script:                              Status:'
 if [ -f /etc/init.d/veapysh-generatorbackups ]; then
-	echo -e "veapysh-generatorbackups" "	     (\e[32mEnabled\e[0m)"
+        echo -e "veapysh-generatorbackups" "         (\e[32mEnabled\e[0m)"
 else
-	echo -e "veapysh-generatorbackups" "            (\e[31mDisabled\e[0m)"
+        echo -e "veapysh-generatorbackups" "            (\e[31mDisabled\e[0m)"
 fi
 
 if [ -f /etc/init.d/veapysh-generatorupgrades ]; then
@@ -19,13 +16,20 @@ if [ -f /etc/init.d/veapysh-generatorupgrades ]; then
 else
         echo -e "veapysh-generatorupgrades" "           (\e[31mDisabled\e[0m)"
 fi       
-echo ""; echo -e "Press" "\e[32mENTER\e[0m" " to finish the process."
 
-i=1
-read OPX
-while [ "$i" = "1" ]; do
-  case "$OPX" in
-  *)
-    i=0
-  esac
-done
+case $1 in
+-l)
+        echo ""
+        ;;
+*)
+        echo ""; echo -e "Press" "\e[32mENTER\e[0m" " to finish the process."
+
+        i=1
+        read OPX
+        while [ "$i" = "1" ]; do
+        case "$OPX" in
+        *)
+        i=0
+        esac
+        done
+esac
